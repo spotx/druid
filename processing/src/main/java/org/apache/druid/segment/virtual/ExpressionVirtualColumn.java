478c74d4ed8f7b5319896d64ef1af9b9d83076dd
tlaird <tlaird@spotx.tv>
2019-11-28 14:19:41 +0000

    Remove virtual Column Changes

--- processing/src/main/java/org/apache/druid/segment/virtual/ExpressionVirtualColumn.java
+++ processing/src/main/java/org/apache/druid/segment/virtual/ExpressionVirtualColumn.java
@@ -105,10 +105,7 @@ public class ExpressionVirtualColumn implements VirtualColumn
   @Override
   public ColumnCapabilities capabilities(String columnName)
   {
-    // Note: Ideally we would only "setHasMultipleValues(true)" if the expression in question could potentially return
-    // multiple values. However, we don't currently have a good way of determining this, so to be safe we always
-    // set the flag.
-    return new ColumnCapabilitiesImpl().setType(outputType).setHasMultipleValues(true);
+    return new ColumnCapabilitiesImpl().setType(outputType);
   }
 
   @Override
